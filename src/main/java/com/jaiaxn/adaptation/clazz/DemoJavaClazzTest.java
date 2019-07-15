528845b1440890dcf2c7ca280b9035a0d08cadf7
wang.jiaxin <wang.jiaxin110@iwhalecloud.com>
2019-07-15 11:31:18 +0800

    配置更新

--- src/main/java/com/jaiaxn/adaptation/clazz/DemoJavaClazzTest.java
+++ src/main/java/com/jaiaxn/adaptation/clazz/DemoJavaClazzTest.java
@@ -11,7 +11,6 @@ import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 import java.util.Map;
 
 /**
--- src/main/java/com/jaiaxn/adaptation/config/JavaCustomizeComponent.java
+++ src/main/java/com/jaiaxn/adaptation/config/JavaCustomizeComponent.java
@@ -5,13 +5,17 @@ import com.jaiaxn.adaptation.dto.AdaptationRequest;
 import java.util.Map;
 
 /**
+ * 定制化java类需实现JavaCustomizeComponent接口，并且必须添加注解：@Service（org.springframework.stereotype.Service）
+ * exec即为默认执行方法
+ * adaptationRequest为页面入参，execSql为数据库配置生成的最终sql
+ *
  * @author: wang.jiaxin
  * @date: 2019年07月08日
  * @description: 自定义执行父类
  **/
 public interface JavaCustomizeComponent {
     /**
-     * 自定义执行类
+     * 自定义执行类-默认执行方法
      * @param adaptationRequest 页面入参
      * @param execSql 数据库配置生成的脚本
      * @return 执行结果
